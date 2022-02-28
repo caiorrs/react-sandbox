8e0c7e6fe5d6456740193e3624bd38282d6aac5b
Caio Roberto <caiorrs@gmail.com>
2022-02-28 17:40:14 -0300

    Added useMemo, useCallback and custom hooks examples

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import CustomHookExample1 from "./components/CustomHookExample1";
+import CustomHookExample2 from "./components/CustomHookExample2";
+import UseCallbackExample from "./components/UseCallbackExample";
+import UseMemoExample from "./components/UseMemoExample";
 import UseRefExample1 from "./components/UseRefExample1";
 import UseRefExample2 from "./components/UseRefExample2";
 import UseRefExample3 from "./components/UseRefExample3";
@@ -7,7 +11,11 @@ function App() {
     <div className="container mt-5">
       {/* <UseRefExample1 /> */}
       {/* <UseRefExample2 /> */}
-      <UseRefExample3 />
+      {/* <UseRefExample3 /> */}
+      {/* <UseMemoExample /> */}
+      {/* <UseCallbackExample /> */}
+      {/* <CustomHookExample1 /> */}
+      <CustomHookExample2 />
     </div>
   );
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url, options) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    loading,
+    error,
+    data,
+  };
+};
+
+export default useFetch;
